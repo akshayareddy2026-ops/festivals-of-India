855b5ab7bc53c61a811264867dfa774047989e99
akshayareddy2026-ops <akshayareddy2026@gmail.com>
2026-03-04 10:59:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-function speakFestival(festival) {
+function typeWriter(text) {
+    let i = 0;
+    let speed = 40;
+    document.getElementById("boardText").innerHTML = "";
+
+    function typing() {
+        if (i < text.length) {
+            document.getElementById("boardText").innerHTML += text.charAt(i);
+            i++;
+            setTimeout(typing, speed);
+        }
+    }
+
+    typing();
+}
+
+function startLesson(festival) {
 
     let text = "";
 
     if (festival === "diwali") {
-        text = "Good morning students. Today we will learn about Diwali. Diwali is celebrated to mark the return of Lord Rama to Ayodhya after defeating Ravana. It symbolizes victory of light over darkness. People wear traditional clothes, light diyas, and celebrate mostly in North India.";
+        text = "Good morning students. Today we are learning about Diwali. Diwali is celebrated to mark the return of Lord Rama to Ayodhya after defeating Ravana. It represents victory of light over darkness. People wear traditional clothes, light diyas, and celebrate grandly in North India.";
     }
 
     if (festival === "holi") {
-        text = "Good morning students. Today we will learn about Holi. Holi is based on the story of Prahlada and Holika. It is the festival of colors. People wear white clothes and celebrate mainly in Uttar Pradesh and North India.";
+        text = "Good morning students. Today we are learning about Holi. Holi is based on the story of Prahlada and Holika. It is the festival of colors. People wear white clothes and celebrate mostly in Uttar Pradesh and North India.";
     }
 
-    document.getElementById("boardText").innerText = text;
+    typeWriter(text);
 
     let speech = new SpeechSynthesisUtterance(text);
     speech.lang = "en-IN";
